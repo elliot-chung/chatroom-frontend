4710715ba4127608ccae55f9e7052a9f142ff79a
Elliot <ec2018@gmail.com>
2023-10-31 20:08:12 -0700

    Added README and fixed some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,63 @@
-import { useEffect, useState } from 'react'
-import Canvas from './components/Canvas'
-import ChatInput from './components/ChatInput'
-import ChatWindow from './components/ChatWindow'
+import { useEffect, useState } from "react";
+import Canvas from "./components/Canvas";
+import ChatInput from "./components/ChatInput";
+import ChatWindow from "./components/ChatWindow";
 
-const wsHost = import.meta.env.VITE_WS_ADDRESS
+const wsHost = "wss://chatroom-backend.fly.dev/chatroom";
 
-const socket = new WebSocket(wsHost)
+const socket = new WebSocket(wsHost);
 
 function App() {
-  const [socketState, setSocketState] = useState("loading")
-  const [user, setUser] = useState<string>('')
-  const [color, setColor] = useState("#000000")
+  const [socketState, setSocketState] = useState("loading");
+  const [user, setUser] = useState<string>("");
+  const [color, setColor] = useState("#000000");
 
   useEffect(() => {
     socket.onopen = () => {
-      console.log("Connected to server")
-      setSocketState("ready")
-    }
+      console.log("Connected to server");
+      setSocketState("ready");
+    };
 
     socket.onclose = () => {
-      console.log("Disconnected from server")
-      setSocketState("error")
-    }
+      console.log("Disconnected from server");
+      setSocketState("error");
+    };
 
     socket.onerror = () => {
-      console.log("Error connecting to server")
-      setSocketState("error")
-    }
-  }, [])
+      console.log("Error connecting to server");
+      setSocketState("error");
+    };
+  }, []);
 
   return (
     <div className="flex flex-col h-screen bg-[#141430]">
-      <h1 className="text-lg font-bold font-mono text-center text-white">Chatroom App</h1>
-      {socketState === "ready" && (<>
-      <Canvas user={user} color={color} socket={socket} />
-      <ChatWindow socket={socket} />
-      <ChatInput user={user} setUser={setUser} color={color} setColor={setColor} socket={socket}/>
-      </>)}
-
-
+      <h1 className="text-lg font-bold font-mono text-center pt-4 text-white">
+        Chatroom App
+      </h1>
+      {socketState === "ready" && (
+        <>
+          <Canvas user={user} color={color} socket={socket} />
+          <ChatWindow socket={socket} />
+          <div className="mb-8">
+            <ChatInput
+              user={user}
+              setUser={setUser}
+              color={color}
+              setColor={setColor}
+              socket={socket}
+            />
+          </div>
+        </>
+      )}
 
       {socketState === "loading" && (
-      <p className="text-center">Connecting...</p> 
+        <p className="text-center">Connecting...</p>
       )}
       {socketState === "error" && (
-      <p className="text-center">Error connecting to server</p>
+        <p className="text-center">Error connecting to server</p>
       )}
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,40 +1,51 @@
-import React, { useEffect } from 'react'
-import ServerMessage from '../types/ServerMessage'
+import React, { useEffect } from "react";
+import ServerMessage from "../types/ServerMessage";
 
 type Props = {
-    socket: WebSocket
-}
+  socket: WebSocket;
+};
 
 const ChatWindow = (props: Props) => {
-    const { socket } = props
-    const [messages, setMessages] = React.useState<{ text: string, user: string, color: string }[]>([])
-    const scrollRef = React.useRef<HTMLDivElement>(null)
-    
-    useEffect(() => {
-        socket.addEventListener("message", (event) => {
-            console.log("Data received from server: ", event.data)
-            const message: ServerMessage = JSON.parse(event.data)
+  const { socket } = props;
+  const [messages, setMessages] = React.useState<
+    { text: string; user: string; color: string }[]
+  >([]);
+  const scrollRef = React.useRef<HTMLDivElement>(null);
 
-            const colorString = "#" + message.Color.toString(16).padStart(6, "0").toUpperCase()
+  useEffect(() => {
+    socket.addEventListener("message", (event) => {
+      const message: ServerMessage = JSON.parse(event.data);
 
-            if (message.Type === 'message') setMessages([...messages, { text: message.Text || "", user: message.User, color: colorString }])
-        })
-    }, [messages, socket])
+      const colorString =
+        "#" + message.Color.toString(16).padStart(6, "0").toUpperCase();
 
-    useEffect(() => {
-        scrollRef.current?.scrollTo(0, scrollRef.current.scrollHeight)
-    }, [messages])
+      if (message.Type === "message")
+        setMessages([
+          ...messages,
+          { text: message.Text || "", user: message.User, color: colorString },
+        ]);
+    });
+  }, [messages, socket]);
 
-    return (
-        <div className="w-4/5 m-auto h-1/5 overflow-y-scroll" ref={scrollRef}>
-            {messages.map((message, index) => (
-                <div className="flex flex-row" key={index}>
-                    <p className="py-4 px-8 w-1/6 truncate" style={{color: message.color }}>{message.user}</p>
-                    <p className="p-4 w-5/6 text-white">{message.text}</p>
-                </div>
-            ))}
+  useEffect(() => {
+    scrollRef.current?.scrollTo(0, scrollRef.current.scrollHeight);
+  }, [messages]);
+
+  return (
+    <div className="w-4/5 m-auto h-1/5 overflow-y-scroll" ref={scrollRef}>
+      {messages.map((message, index) => (
+        <div className="flex flex-row" key={index}>
+          <p
+            className="py-4 px-8 w-1/6 truncate"
+            style={{ color: message.color }}
+          >
+            {message.user}
+          </p>
+          <p className="p-4 w-5/6 text-white">{message.text}</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default ChatWindow
+export default ChatWindow;
